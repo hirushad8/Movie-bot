e85359b85502c8f8d6ff9605c09ca841e6f0b475
hirushad8 <91839102+hirushad8@users.noreply.github.com>
2024-04-28 23:08:54 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 url_list = {}
-api_key = "e59cb11de157619aee585ddb38129d860ea455a6"
+api_key = "345a07409caab56d2bd985ef621d5de82d36ae0e"
 
 
 def search_movies(query):
